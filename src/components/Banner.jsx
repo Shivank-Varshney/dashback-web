7da32f19746a0df65a493d15814f8feb74e82614
Shivank Varshney <shivank1703@gmail.com>
2021-06-02 15:04:26 +0530

    banner 3 added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,6 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Img1 from "../assets/banner-1.svg";
 import Img2 from "../assets/banner-2.svg";
+import Img3 from "../assets/banner-3.svg";
+import {NavLink} from "react-router-dom";
 
 const Banner = () => {
     var settings = {
@@ -12,63 +14,59 @@ const Banner = () => {
         speed: 2000,
         slidesToShow: 1,
         arrows: false,
-        autoplay: true,
+        autoplay: false,
         className: "slider",
     };
 
     return(
         <>
             <Slider {...settings}>
-                {
-                    BannerSlideData.map((val,index)=>{
-                        return(
-                            <BannerSlide
-                                key = {index}
-                                imgsrc = {val.imgsrc}
-                                heading = {val.heading}
-                                content = {val.content}
-                                class = {val.class}
+                <div className="row">
+                    <div className="col-12">
+                        <div className="row banner-1">
+                            <div className="col-md-6 d-flex banner-1-bg">
+                                <img src={Img1} className="img-fluid mx-auto" alt="" />
+                            </div>
+                            <div className="col-md-6 banner-1-text">
+                                <h3>Get upto 100% cash back on all your utility bills</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                            />
-                        );
-                    })
-                }
+                <div className="row">
+                    <div className="col-12">
+                        <div className="row banner-2">
+                            <div className="col-md-6 d-flex banner-2-bg">
+                                <img src={Img2} className="img-fluid mx-auto" alt="" />
+                            </div>
+                            <div className="col-md-6 banner-2-text">
+                                <h3>Shop on ecommerce brands throgh us and save upto 100% on each transaction</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-12">
+                        <div className="row banner-3">
+                            <div className="col-md-6 d-flex banner-3-bg">
+                                <img src={Img3} className="img-fluid mx-auto" alt="" />
+                            </div>
+                            <div className="col-md-6 banner-3-text">
+                                <h4>dashback presents</h4>
+                                <h3>How to Save <br /> while <br /> Spending?</h3>
+                                <h5>Fill your DASH with CASH</h5>
+                                <NavLink to="/webinar#" className="btn-webinar">live <br /> webinar</NavLink>
+                                <p>JUNE 6,2021 <br /> 06:00 PM</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Slider>
         </>
     );
 }
 
-const BannerSlide = (props) => {
-    return(
-        <>
-            <div className="row mx-auto">
-                    <div className={`col-md-6 ${props.class}`}>
-                        <img src={props.imgsrc} alt="" className="img-fluid" />
-                    </div>
-                    <div className="col-md-6 banner-text">
-                        <h1>{props.heading}</h1>
-                        {/* <p>{props.content}</p>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt tenetur unde asperiores pariatur! Iste accusamus molestias repellat iusto, numquam officiis incidunt necessitatibus debitis nulla earum cupiditate asperiores.</p>
-                        <div className="get-app-btn">get app</div> */}
-                    </div>
-                </div>
-        </>
-    );
-}
-
-const BannerSlideData = [
-    {
-        imgsrc: Img1,
-        heading: "Get upto 100% cash back on all your utility bills",
-        content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt tenetur unde asperiores pariatur! Iste accusamus molestias repellat iusto, numquam officiis incidunt necessitatibus debitis nulla earum cupiditate asperiores.",
-        class: "banner-1"
-    },
-    {
-        imgsrc: Img2,
-        heading: "Shop on ecommerce brands throgh us and save upto 100% on each transaction",
-        content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt tenetur unde asperiores pariatur! Iste accusamus molestias repellat iusto, numquam officiis incidunt necessitatibus debitis nulla earum cupiditate asperiores.",
-        class: "banner-2"
-    },
-];
 
 export default Banner;
